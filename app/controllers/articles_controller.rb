53544c5b61f4686778055ebf1a1cfed85f12c709
rylandgrounds <rylandgrounds@gmail.com>
2020-07-13 10:18:45 -0500

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,7 +10,14 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
-
+  def edit
+    @article = Article.find(params[:id])
+  end
+  def update
+    @article = Article.find(params[:id])
+    @article.update(title: params[:article][:title], description: params[:article][:description])
+    redirect_to article_path(@article)
+  end
   def create
     @article = Article.new
     @article.title = params[:title]
